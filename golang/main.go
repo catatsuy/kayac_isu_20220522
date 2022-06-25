95b5def0ed43fdeba5755dc9b3e4f5326d21f79a
catatsuy <catatsuy@catatsuy.org>
2022-06-25 16:21:05 +0900

    use isDev

--- golang/main.go
+++ golang/main.go
@@ -21,6 +21,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	"github.com/oklog/ulid/v2"
+	proxy "github.com/shogo82148/go-sql-proxy"
 	"github.com/srinathgs/mysqlstore"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -37,6 +38,7 @@ var (
 	tr           = &renderer{templates: template.Must(template.ParseGlob("views/*.html"))}
 	// for use ULID
 	entropy = ulid.Monotonic(rand.New(rand.NewSource(time.Now().UnixNano())), 0)
+	isDev   bool
 )
 
 func getEnv(key string, defaultValue string) string {
@@ -58,6 +60,11 @@ func connectDB() (*sqlx.DB, error) {
 	config.ParseTime = true
 
 	dsn := config.FormatDSN()
+	if isDev {
+		proxy.RegisterTracer()
+
+		return sqlx.Open("mysql:trace", dsn)
+	}
 	return sqlx.Open("mysql", dsn)
 }
 
@@ -81,9 +88,13 @@ var songULIDs map[string]SongRow
 var artistIDs map[int]ArtistRow
 
 func main() {
+	if os.Getenv("DEV") == "1" {
+		isDev = true
+	}
+
 	e := echo.New()
-	e.Debug = true
-	e.Logger.SetLevel(log.DEBUG)
+	e.Debug = false
+	e.Logger.SetLevel(log.ERROR)
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
